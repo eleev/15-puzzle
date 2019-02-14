82f44e63bc89932302daa709e0620506471e6474
Astemir Eleev <astemireleev@gmail.com>
2019-02-14 21:09:31 +0300

    Initial Commit

---
+++ ios-15-puzzleTests/ios_15_puzzleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_15_puzzleTests.swift
+//  ios-15-puzzleTests
+//
+//  Created by Astemir Eleev on 14/02/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import XCTest
+@testable import ios_15_puzzle
+
+class ios_15_puzzleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
