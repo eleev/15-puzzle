c852edf2f2df3c3172da158d3a2255a158299a15
Astemir Eleev <astemireleev@gmail.com>
2019-02-15 17:33:18 +0300

    Refactored support for interface orientation too .portrait

--- ios-15-puzzle/ViewControllers/GameViewController.swift
+++ ios-15-puzzle/ViewControllers/GameViewController.swift
@@ -37,11 +37,7 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .portrait
     }
 
     override var prefersStatusBarHidden: Bool {
