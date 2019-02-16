a49206f4c3f078fbba419ea0d6dbef269151bc47
Astemir Eleev <astemireleev@gmail.com>
2019-02-16 16:15:03 +0300

    Implemented NodeType enum type

---
+++ ios-15-puzzle/Models/Node Factory/NodeType.swift
@@ -0,0 +1,13 @@
+//
+//  NodeType.swift
+//  ios-15-puzzle
+//
+//  Created by Astemir Eleev on 15/02/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+enum NodeType {
+    case slot(numbered: UInt)
+}
