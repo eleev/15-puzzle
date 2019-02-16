e460c5d10b251351cbfc10ad48579c2e8bb53973
Astemir Eleev <astemireleev@gmail.com>
2019-02-16 16:14:52 +0300

    Implemented NodeFactory struct

---
+++ ios-15-puzzle/Models/Node Factory/NodeFactory.swift
@@ -0,0 +1,21 @@
+//
+//  NodeFactory.swift
+//  ios-15-puzzle
+//
+//  Created by Astemir Eleev on 15/02/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import CoreGraphics
+
+struct NodeFactory {
+    
+    static func produce(of type: NodeType, position: CGPoint) -> GenericNodeType {
+        switch type {
+        case .slot(let number):
+            let node = SlotNode(number: number, size: Constants.cellSize)
+            node.position = position
+            return node
+        }
+    }
+}
