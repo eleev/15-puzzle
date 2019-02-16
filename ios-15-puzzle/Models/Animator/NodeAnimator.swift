70b0aa94bb65c6090eeea0a3076de4a1d0252690
Astemir Eleev <astemireleev@gmail.com>
2019-02-16 16:13:29 +0300

    Implemented NodeAnimator struct

---
+++ ios-15-puzzle/Models/Animator/NodeAnimator.swift
@@ -0,0 +1,35 @@
+//
+//  NodeAnimator.swift
+//  ios-15-puzzle
+//
+//  Created by Astemir Eleev on 15/02/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+struct NodeAnimator {
+    
+    static func reveal(node: GenericNodeType, duration: TimeInterval) {
+        node.alpha = 0.0
+        let unhide = SKAction.fadeAlpha(to: 1.0, duration: duration)
+        node.run(unhide)
+    }
+    
+    static func alpha(node: SKSpriteNode, for alpha: CGFloat, duration: TimeInterval) {
+        node.run(SKAction.fadeAlpha(to: alpha, duration: duration))
+    }
+    
+    struct ShuffleNode {
+        
+        static func fadeOut(node: SKSpriteNode?) {
+            guard let node = node else { return }
+            NodeAnimator.alpha(node: node, for: Constants.shuffleButtonDimLevel, duration: Constants.shuffleButtonDimAnimationDuration)
+        }
+        
+        static func fadeIn(node: SKSpriteNode?) {
+            guard let node = node else { return }
+            NodeAnimator.alpha(node: node, for: 1.0, duration: Constants.shuffleButtonDimAnimationDuration)
+        }
+    }
+}
