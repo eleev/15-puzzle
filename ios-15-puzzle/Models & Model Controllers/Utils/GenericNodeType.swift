7a26f83a72a549b0c9ae713f9bef59f06266c5c7
Astemir Eleev <astemireleev@gmail.com>
2019-02-17 13:31:42 +0300

    Created GeneticNodeType type alias

---
+++ ios-15-puzzle/Models & Model Controllers/Utils/GenericNodeType.swift
@@ -0,0 +1,11 @@
+//
+//  GenericNodeType.swift
+//  ios-15-puzzle
+//
+//  Created by Astemir Eleev on 15/02/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+typealias GenericNodeType = SKSpriteNode & Numberable & Slidable
